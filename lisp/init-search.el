cbb63ee69816d92ebe36656da0c1ee801a017979
Luke Green <luke@pop-os.localdomain>
2020-05-20 23:38:18 -0500

    add init-search.el

---
+++ lisp/init-search.el
@@ -0,0 +1,13 @@
+;;; init-search.el --- Isearch configs -*- lexical-binding: t -*-
+;;; Commentary:
+;;  Change some isearch stuff, install anzu
+;;; Code:
+(with-eval-after-load 'isearch
+  (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char))
+
+(jlgre/require 'anzu)
+
+(add-hook 'after-init-hook 'anzu)
+
+(provide 'init-search)
+;;; init-search.el ends here
